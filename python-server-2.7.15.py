4207f13166a167a5dee5df103b5b96160e962dce
Mbrookesy <milobrookes0@gmail.com>
2019-11-07 11:24:16 +0000

    Testing Docker

--- python-server-2.7.15.py
+++
@@ -1,7 +0,0 @@
-import SimpleHTTPServer
-import SocketServer
-
-PORT = 9000
-Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
-httpd = SocketServer.TCPServer(("", PORT), Handler)
-httpd.serve_forever()
